b5a92ab71a92fadc3bf58aeba6a185071f92b89e
devinbayly <baylyd@email.arizona.edu>
2022-06-20 11:27:59 -0700

    make 500 plants then stop

--- godot_files/exercise_getting_ray_intersection.gd
+++ godot_files/exercise_getting_ray_intersection.gd
@@ -10,19 +10,19 @@ onready var plant = load("res://plant.tscn")
 # Called when the node enters the scene tree for the first time.
 
 func doThing(res):
-	print(res)
+	##print(res)
 	## make instance of plant
 	var plant_instance = plant.instance()
 	plant_instance.translate(res)
 	plant_instance.scale_object_local(Vector3(scaleNum,scaleNum,scaleNum))
 	add_child(plant_instance)
 	
+var plants_number = 500
 
-var place =false
 
 func _physics_process(delta):
-	if place:
-		place = false
+	if plants_number > 0:
+		plants_number -=1
 		var state = get_world().direct_space_state
 		var xz = Vector3(randf(),0,randf())*aabb.size + aabb.position
 		var from = Vector3(xz.x,100,xz.z)
@@ -31,7 +31,3 @@ func _physics_process(delta):
 		doThing(result.position)
 	
 	
-func _input(event):
-	if event is InputEventMouseButton:
-		print(event.to_string())
-		place = true
